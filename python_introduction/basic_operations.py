22625ae5f225a7e79bbb8b4f53f41c541833dd36
jabezdubale <jabezdubale6@gmail.com>
2025-09-19 14:35:43 +0300

    0. Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+number1 = 10
+number2 = 5
+print("sum of", number1, "and", number2, "is", number1+number2)
+print("difference of", number1, "and", number2, "is", number1-number2)
+print("Multiplication of", number1, "and", number2, "is", number1*number2)
