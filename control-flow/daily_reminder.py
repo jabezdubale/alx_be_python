8360c2d73fe62a91252cd298a0260d91858475b3
jabezdubale <jabezdubale6@gmail.com>
2025-09-21 23:01:56 +0300

    4. Personal Daily Reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,18 +5,18 @@ time_bound = input ("Is it time-bound? (yes/no): ")
 match priority:
     case "high":
         if time_bound == "yes":
-            print(f"'{task}' is a {priority} priority task that requires immediate attention today")
+            print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today")
         else:
-            print(f"'{task}' is a {priority} priority. Consider completing it when you have free time.")
+            print(f"Reminder: '{task}' is a {priority} priority. Consider completing it when you have free time.")
     case "medium":
         if time_bound == "yes":
-            print(f"'{task}' is a {priority} priority task that requires immediate attention today")
+            print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today")
         else:
-            print(f"'{task}' is a {priority} priority. Consider completing it when you have free time.")
+            print(f"Reminder: '{task}' is a {priority} priority. Consider completing it when you have free time.")
     case "low":
         if time_bound == "yes":
-            print(f"'{task}' is a {priority} priority task that requires immediate attention today")
+            print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today")
         else:
-            print(f"'{task}' is a {priority} priority. Consider completing it when you have free time.")
+            print(f"Reminder: '{task}' is a {priority} priority. Consider completing it when you have free time.")
     case _:
         print("There is no such type of priority")
